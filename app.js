fbbca4337491e549a32782e8721921d913e80427
Sabali11 <101278445+Sabali11@users.noreply.github.com>
2025-08-26 04:00:29 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 import express, { urlencoded } from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+
 import authRoute from "./routes/authRoutes.js"
 import userRoute from "./routes/userRoutes.js"
 import postRoute from "./routes/postRoutes.js"
 import commentRoute from "./routes/commentRoutes.js"
 import errorHandler from "./middleware/errorMiddleware.js"
-import dotenv from "dotenv"
 import connectDB from "./config/db.js";
-import cors from "cors"
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5000
+connectDB()
 
 app.use(cors())
 app.use(express.json())
-app.use(errorHandler)
 app.use(urlencoded({extended : true}))
 
 
@@ -24,7 +25,12 @@ app.use("/api/user", userRoute)
 app.use("/api/posts", postRoute)
 app.use("/api/comment", commentRoute)
 
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.use(errorHandler)
+
 app.listen(PORT, () => {
-    connectDB()
     console.log(`app started on ${PORT}`)
 })
